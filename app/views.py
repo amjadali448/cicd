1f4a01fcb7779ea03dace496369fa322778c9317
Amjad Ali <66878467+amjadali448@users.noreply.github.com>
2025-01-31 19:07:05 +0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def helloworld(request):
-    return HttpResponse("<html> <body> Hello Abdur Rehman </body> </html>")
+    return HttpResponse("<html> <body> Hello Abdur Rehman is good </body> </html>")
 
